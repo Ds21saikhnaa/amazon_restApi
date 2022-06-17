aea1840cf9cfbd943a311c3e3e7ff6c67a7f1e97
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-06-17 23:24:43 +0800

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import  express  from "express";
 import  dotenv from "dotenv";
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from 'url';
 import morgan from "morgan";
+import {connectDB} from "./config/db.js"
 import rotatingFileStream from "rotating-file-stream"
 import catRouter from "./router/categories.js";
 import {logger} from "./middleware/logger.js";
+import colors from "colors";
 dotenv.config({path: "./config/config.env"});
+connectDB();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__dirname);
@@ -20,4 +22,10 @@ const app = express();
 app.use(logger);
 app.use(morgan('combined', { stream: accessLogStream }))
 app.use("/api/v1/categories/",catRouter);
-app.listen(process.env.PORT, console.log(`hello ${process.env.PORT} server`));
+const server =  app.listen(process.env.PORT, console.log(`hello ${process.env.PORT} server`.underline.yellow.bold));
+process.on("ubhandledRejection", (err, promise) => {
+    console.log(`aldaa garsaan: ${err.message}`.underline.red.bold);
+    server.close(() => {
+        process.exit(1);
+    });
+});
