f3dd48f833dbd484d4638d6f5e66344e44950e43
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-06-20 01:19:57 +0800

    aldaani messange hiiw

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,11 +1,16 @@
  export const errorHandler = (err, req, res, next) => {
-    console.log(err.stack.blue);
-    if(err.name === "CastError"){
-        err.message = "ene ID buruu butetstei bn!";
-        err.statusCode = 400;
-    }
-    res.status(err.statusCode || 500).json({
+     console.log(err.stack.blue);
+     const error = {...err};
+    if(error.name === "CastError"){
+        error.message = "ene ID buruu butetstei bn!";
+        error.statusCode = 400;
+    };
+    if(error.code === 11000){
+        error.message = "ene talbariin utga ali hedin bn!";
+        error.statusCode = 400;
+    };
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err,
-    })
+        error: error,
+    });
 }
